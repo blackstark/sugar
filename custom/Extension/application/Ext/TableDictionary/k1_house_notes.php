731697eccaa160075cf3b6ea9a3ecf4b59207a9e
stark <stark90@mail.com>
2015-05-26 22:44:45 +0700

    1.002

--- custom/Extension/application/Ext/TableDictionary/k1_house_notes.php
+++
@@ -1,5 +0,0 @@
-<?php 
- //WARNING: The contents of this file are auto-generated
-include('custom/metadata/k1_house_notesMetaData.php');
-
-?>
--- custom/Extension/modules/Notes/Ext/Language/en_us.House.php
+++
@@ -1,3 +0,0 @@
-<?php
-//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
-$mod_strings['LBL_K1_HOUSE_NOTES_FROM_K1_HOUSE_TITLE'] = 'Объекты недвижимости';
